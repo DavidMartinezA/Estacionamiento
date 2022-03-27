8b7b71800a7f18a88c3d682e7924294603fb4211
cristian.martinez <davick16@gmail.com>
2022-03-26 23:36:29 -0500

    dependencia mockito en dominio

---
+++ dominio/src/test/java/com/example/modelo/usuario/servicio/ServicioUsuarioTest.kt
@@ -0,0 +1,22 @@
+package com.example.modelo.usuario.servicio
+
+import org.junit.Assert.*
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(MockitoJUnitRunner::class)
+class ServicioUsuarioTest {
+
+    @Test
+    fun guardar() {
+    }
+
+    @Test
+    fun eliminar() {
+    }
+
+    @Test
+    fun consultarLista() {
+    }
+}
